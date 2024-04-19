30c78ea7b10d90e2cc117c02f5f61604f2c87d99
1thenameisshivam <1thenameisshivam@gmail.com>
2024-04-20 01:00:28 +0530

    add delete endpoint

--- src/books/bookRoute.ts
+++ src/books/bookRoute.ts
@@ -4,6 +4,7 @@ import {
   updateBook,
   listBooks,
   getSingleBook,
+  deleteBook,
 } from "./bookController";
 import multer from "multer";
 import path from "node:path";
@@ -34,7 +35,7 @@ bookRouter.put(
   ]),
   updateBook
 );
-
+bookRouter.delete("/:bookId", authenticate, deleteBook);
 bookRouter.get("/", listBooks);
 bookRouter.get("/:bookId", getSingleBook);
 
