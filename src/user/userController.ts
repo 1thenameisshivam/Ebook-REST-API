b841a8696ca849cf788516784cfbada66800b76e
1thenameisshivam <1thenameisshivam@gmail.com>
2024-04-19 13:54:48 +0530

    add login controller and route with error handler

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -52,10 +52,57 @@ const registerUser = async (
       expiresIn: "7d",
       algorithm: "HS256",
     });
-    res.json({ message: "user registered successfully", access_token: token });
+    res
+      .status(201)
+      .json({ message: "user registered successfully", access_token: token });
   } catch (err) {
     return next(createHttpError(500, "error while generating token"));
   }
 };
 
-export { registerUser };
+const loginUser = async (req: Request, res: Response, next: NextFunction) => {
+  const { email, password } = req.body;
+
+  //validating the user
+  if (!email || !password) {
+    const error = createHttpError(400, "please provide all the fields");
+    return next(error);
+  }
+
+  //check user exist or not
+
+  let user;
+
+  try {
+    user = await userModel.findOne({ email });
+    if (!user) {
+      return next(createHttpError(400, "user not found with this email"));
+    }
+  } catch (err) {
+    return next(createHttpError(500, "error while getting user"));
+  }
+
+  // compare the password
+  try {
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+    if (!isPasswordMatch) {
+      return next(createHttpError(400, "password not match"));
+    }
+  } catch (err) {
+    return next(createHttpError(500, "error while comparing(bcrypt) password"));
+  }
+
+  //jwt token will be here
+  try {
+    const token = sign({ sub: user._id }, config.jwtSecret, {
+      expiresIn: "7d",
+    });
+    res
+      .status(200)
+      .json({ message: "login successfully", access_token: token });
+  } catch (err) {
+    return next(createHttpError(500, "error while generating token"));
+  }
+};
+
+export { registerUser, loginUser };
